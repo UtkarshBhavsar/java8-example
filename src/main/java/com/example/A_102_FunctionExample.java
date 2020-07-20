086e0514898af27469403d2a9bd2e51c084343be
UtkarshBhavsar <ubhavsar@mobiquityinc.com>
2020-07-20 21:35:20 +0530

    A_102_Demo.java Function example and chaining

---
+++ src/main/java/com/example/A_102_FunctionExample.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import java.util.function.Function;
+
+public class A_102_FunctionExample {
+
+    public static void main(String[] args) {
+        // Imperative: 1 increment the value
+        System.out.println("Following is the output of Imperative approach");
+        System.out.println(increment(0));
+
+        // Declarative: 1 Function example & chaining
+        System.out.println("Following is the output of Declarative approach");
+        int value = 2;
+        System.out.println("Initial value = " + value);
+        System.out.println("Incremented by 1 = " + incrementFunction.apply(value));
+        System.out.println("Incremented by 1 and square = " + incrementFunction
+                .andThen(squareFunction)
+                .apply(value));
+        System.out.println("Incremented by 1 and square and square root = " + incrementFunction
+                .andThen(squareFunction)
+                .andThen(squareRootFunction)
+                .apply(value));
+        System.out.println("Incremented by 1 and square and square root and decrement by 1 =  "
+                + incrementFunction
+                .andThen(squareFunction)
+                .andThen(squareRootFunction)
+                .andThen(decrementFunction)
+                .apply(value));
+    }
+
+    static Function<Integer, Integer> incrementFunction = val -> val + 1;
+    static Function<Integer, Integer> decrementFunction = val -> val - 1;
+    static Function<Integer, Integer> squareFunction = val -> val * val;
+    static Function<Integer, Integer> squareRootFunction = val -> (int) Math.sqrt((double) val);
+
+    static int increment(int increment) {
+        return increment + 1;
+    }
+}
