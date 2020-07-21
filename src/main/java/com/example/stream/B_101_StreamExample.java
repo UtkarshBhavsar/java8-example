27c10265bffbba135802004c17d5016e056c4746
UtkarshBhavsar <ubhavsar@mobiquityinc.com>
2020-07-21 20:11:42 +0530

    B_101_StreamExample Stream more example

--- src/main/java/com/example/stream/B_101_StreamExample.java
+++ src/main/java/com/example/stream/B_101_StreamExample.java
@@ -13,15 +13,26 @@ public class B_101_StreamExample {
                 new Person("Marley", MALE),
                 new Person("Sara", FEMALE),
                 new Person("James", MALE),
-                new Person("Maria", FEMALE),
-                new Person("Kelly", OTHER)
+                new Person("Maria", FEMALE)
+//                new Person("Kelly", OTHER)
         );
-        personList.stream()
-//                .map(Person::getGender)
+        /*personList.stream()
                 .map(Person::getName)
                 .mapToInt(String::length)
-//                .distinct()
-                .forEach(System.out::println);
+                .forEach(System.out::println);*/
+        boolean allMatchFemales = personList.stream().allMatch(person -> person.getGender() == FEMALE);
+        System.out.println(allMatchFemales);
+        boolean anyMatchFemales = personList.stream().anyMatch(person -> person.getGender() == FEMALE);
+        System.out.println(anyMatchFemales);
+        boolean noneMatchFemales = personList.stream().noneMatch(person -> person.getGender() == OTHER);
+        System.out.println(noneMatchFemales);
+
+        personList.stream()
+                .takeWhile(person -> person.getGender() == MALE)
+                .filter(person -> person.getName().startsWith("BAA"))
+                .findAny()
+                .ifPresentOrElse(System.out::println, () -> System.out.println("Data not found"));
+
     }
 
 
