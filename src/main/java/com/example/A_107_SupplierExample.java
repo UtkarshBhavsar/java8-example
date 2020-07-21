3ce54936d8ce2902353e737de14f65ddd44bffd2
UtkarshBhavsar <ubhavsar@mobiquityinc.com>
2020-07-21 19:43:05 +0530

    A_107_SupplierExample Supplier Example

---
+++ src/main/java/com/example/A_107_SupplierExample.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.function.Supplier;
+
+public class A_107_SupplierExample {
+
+    public static void main(String[] args) {
+        // Imperative: Check phone number is valid
+        System.out.println("Following is the output of Imperative approach");
+        System.out.println(getDBUrl());
+        
+        // Declarative: Check phone number is valid
+        System.out.println("\nFollowing is the output of Declarative approach");
+        System.out.println(getDbUrlSupplier.get());
+    }
+
+    static String getDBUrl() {
+        return "jdbc://localhost:3306/user";
+    }
+
+    static Supplier<String> getDbUrlSupplier = () -> "jdbc://localhost:3306/user";
+}
